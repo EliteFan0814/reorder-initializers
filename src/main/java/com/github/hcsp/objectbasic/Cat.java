ebce35c42cf57c1aa5f1677be7e68014195e26da
EliteFan <fan552426811@outlook.com>
2022-08-13 09:44:24 +0800

    按顺序输出

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,41 +1,43 @@
 package com.github.hcsp.objectbasic;
 
-public class Cat {
-    public static int count = initStaticCount();
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
+public class Cat {
     static {
         System.out.println(1);
     }
 
-    int age = initAge();
-    String name;
-
-    {
+    static{
         System.out.println(2);
     }
 
-    {
+    static{
         System.out.println(3);
     }
+    int age = initAge();
+    public static int count = initStaticCount();
+
+    String name;
+
 
     public int initAge() {
-        System.out.println(4);
+        System.out.println(5);
         return 0;
     }
 
     public static int initStaticCount() {
-        System.out.println(5);
+        System.out.println(4);
         return 0;
     }
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
